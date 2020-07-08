89eb074b4c452331816ed48e1b06e3a3a1bbe0d0
Ji Shan <uug54332017@gmail.com>
2020-07-09 09:45:20 +1000

    edited editPosts

---
+++ src/components/EditPost.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class EditPost extends Component {
+  state = {  }
+  onInputChange = (event) => {
+    
+  }
+  render() { 
+    return ( 
+      <div className="container" >
+        
+      </div>
+     );
+  }
+}
+ 
+export default EditPost;
