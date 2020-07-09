4a574372c2c2cd175935e86478e940081bb4186a
Ji Shan <uug54332017@gmail.com>
2020-07-09 22:05:20 +1000

    added moment and edited show form

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -1,37 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import moment from "moment";
 
 class ShowPost extends Component {
+  render() {
+    const post = this.props.location.state;
+    return (
+      <div className="card mb-3">
+        <img className="card-img-top" src={post.image} alt={post.location} />
+        <div className="card-body">
+          <h5 className="card-title">{post.location}</h5>
+          <h5 className="card-title">{post.country}</h5>
+          <p className="card-text">{post.haunted_level}</p>
+          <p className="card-text">{post.time}</p>
+          <p className="card-text">{post.description}</p>
 
-  render() { 
-  const post = this.props.location.state;
-    return ( 
-    <>
-      <h3>{post.location}</h3>
-      <p>{post.country}</p>
-      <p>{post.haunted_level}</p>
-      <p>{post.time}</p>
-      <p>{post.description}</p>
-    </>
-     );
+          <p className="card-text">
+            <small className="text-muted">Created {moment(post.created_at).startOf("minute").fromNow()}</small>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
- 
+
 export default ShowPost;
 
-// const ShowPost = (props) => {
-//   const post = props.post.location.state;
-//   return ( 
-//     <>
-//     <h3>{post.location}</h3>
-//     <p>{post.country}</p>
-//     <p>{post.haunted_level}</p>
-//     <p>{post.time}</p>
-//     <p>{post.description}</p>
 
-
-
-//     </>
-//    );
-// }
- 
-// export default ShowPost;
